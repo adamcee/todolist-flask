3f96d83fb543f31267dbe4671c07c8f79c6a9552
Adam Cahan <adam.cahan@gmail.com>
2023-06-29 12:15:58 -0500

    Add basic mysql config/init stuff

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask, jsonify
+from flaskext.mysql import MySQL
 
 app = Flask(__name__)
 
-app.config['MY_SPECIAL_CONFIGURATION_VAR'] = 'hello world'
+
+# TODO: MYSQL CONFIG VALUES NOT CORRECT FOR WORKING WITH DEFAULT MYSQL DOCKER IMAGE
+# APP TALKS TO MYSQL SERVER BUT CONNECTION REFUSED
+# Configure mysql database
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = 3306
+app.config['MYSQL_DATABASE_DB'] = 'todo'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+
+mysql = MySQL()
+mysql.init_app(app)
+connection = mysql.connect()
+connection.autocommit(True)
+cursor = connection.cursor()
 
 print(app.config['MY_SPECIAL_CONFIGURATION_VAR'])
 
